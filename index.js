67358da3062e8813126e8c5993212b11f3f4790f
Brian Tom <brian.animet.world>
2018-04-11 04:04:51 +0000

    added support for https

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').load();
 
+const https = require('https');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const express = require('express');
@@ -16,9 +18,25 @@ app.get('/', function(request, response) {
   response.send(tokenGenerator(identity, room));
 });
 
+app.get('*', function(req, res) {
+   res.redirect('https://' + req.headers.host + req.url);
+});
+
 // Create an http server and run it
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 server.listen(port, function() {
   console.log('Express server running on *:' + port);
+});
+
+// Setup https server and run it
+const options = {
+    cert: fs.readFileSync('/etc/letsencrypt/live/twilio-access.animet-prod.com/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/twilio-access.animet-prod.com/privkey.pem')
+};
+
+const httpsServer = https.createServer(options, app);
+const httpsPort = 3443;
+httpsServer.listen(httpsPort, function(){
+  console.log('Express server running https on *:' + httpsPort)
 });
